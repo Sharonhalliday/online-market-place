853d42c85f90d996d9adfcaad8759288633b9270
sharonhalliday <hallidaysharon5@gmail.com>
2023-12-15 12:30:46 -0800

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $("#formOne").submit(function(event) {
+      event.preventDefault();
+        const nameInput = $("input#name").val();
+        const addressInput = $("input#address").val();
+        // const dateInput = $("input#date").val();
+        
+        // $(".name").text(nameInput);
+        // $(".address").text(addressInput);
+        // $(".date").text(dateInput);
+     
+        // const Amount = $("#Amount").val();
+        // $(".Amount").text(Amount);
+
+
+        // const jewelry = $("input:radio[name=jewelry]:checked").val();
+        // $("#radio").text(jewelry);
+
+
+        // $("#Receipt").show();
+    });
+});
